788baee2f55486cb4e850b7307e8f050c4a965fa
Grzegorz Kastelik <g.kastelik@gmail.com>
2017-06-08 22:37:27 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/route.js
+++ js/route.js
@@ -1,25 +1,25 @@
-var app = angular.module("CZKApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl: "/templates/mainDashboard.html",
-            controller: "mainDashboardController"
-        })
-        .when("/templatkazprzyciskiem", {
-            templateUrl: "templates/templatkazprzyciskiem.html"
-        })
-        .when("/badPerm", {
-            templateUrl: "templates/badPerm.html"
-        })
-        .when("/myProfile", {
-            templateUrl: "templates/myProfile.html",
-            controller: "accountController"
-        })
-        .when("/tab", {
-            templateUrl: "templates/tab.html",
-            controller: "tabController"
-        })
-        .otherwise({
-            templateUrl: "templates/badPerm.html"
-        });
+var app = angular.module("CZKApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "/templates/mainDashboard.html",
+            controller: "mainDashboardController"
+        })
+        .when("/templatkazprzyciskiem", {
+            templateUrl: "templates/templatkazprzyciskiem.html"
+        })
+        .when("/badPerm", {
+            templateUrl: "templates/badPerm.html"
+        })
+        .when("/myProfile", {
+            templateUrl: "templates/myProfile.html",
+            controller: "accountController"
+        })
+        .when("/tab", {
+            templateUrl: "templates/tab.html",
+            controller: "tabController"
+        })
+        .otherwise({
+            templateUrl: "templates/badPerm.html"
+        });
 });
