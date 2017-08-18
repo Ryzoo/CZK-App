5899891369a8578d06be0dcd3fbc344fba6f0b81
Grzegorz Kastelik <g.kastelik@gmail.com>
2017-08-18 14:02:59 +0200

    New todo color system

--- modules/Todo/assets/controllers/todoController.js
+++ modules/Todo/assets/controllers/todoController.js
@@ -1,174 +1,68 @@
-app.controller('todoController', function($scope, auth, $rootScope) {
+app.controller('todoController', function($scope, auth, $rootScope, request, notify) {
     $scope.todoList;
-    var basicColor = '#f44336';
+    $scope.selectedColor = '#ff0000';
+    $scope.selectedKey = 1;
 
     $scope.initToDo = function() {
         getMyTodo();
+        setInputColor();
     }
 
     $scope.addTodo = function() {
         var titlea = $('#todoText').val();
-        var colora = $('#todoColor') ? $('#todoColor').val() : basicColor;
+        var colora = $scope.selectedColor;
 
         if (titlea.length <= 3) {
-            $.gritter.add({
-                title: 'Walidacja',
-                text: 'Wpisz dłuższy tekst',
-                image: '',
-                sticky: true,
-                time: 5,
-                class_name: 'my-sticky-class'
-            });
+            notify.localNotify('Walidacja', "Wpisz dłuższy tekst");
             return;
         }
 
         if (titlea.length > 100) {
-            $.gritter.add({
-                title: 'Walidacja',
-                text: 'Wpisz mniej tekstu',
-                image: '',
-                sticky: true,
-                time: 5,
-                class_name: 'my-sticky-class'
-            });
+            notify.localNotify('Walidacja', "Wpisz mniej tekstu");
             return;
         }
-
-        var dataToSend = { token: Cookies.get('tq'), usid: $rootScope.user.id, title: titlea, color: colora };
-        var urlToPost = 'backend/addTodo';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $.gritter.add({
-                        title: 'Sukces',
-                        text: 'Twoje zadanie zostało dodane',
-                        image: '',
-                        sticky: true,
-                        time: 5,
-                        class_name: 'my-sticky-class'
-                    });
-                    $('#todoText').val('');
-                    $('#todoColor').val('#f44336');
-                    getMyTodo();
-                } else {
-                    console.log(msg);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Nie udało się dodać zadania',
-                        image: '',
-                        sticky: true,
-                        time: 5,
-                        class_name: 'my-sticky-class'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety nie udało się pobrać Twoich todo',
-                    image: '',
-                    sticky: true,
-                    time: 5,
-                    class_name: 'my-sticky-class'
-                });
-            },
-        });
+        var dataToSend = { usid: $rootScope.user.id, title: titlea, color: colora };
+        request.backend('addTodo', dataToSend, function() {
+            $('#todoText').val('');
+            $('#todoColor').val('#f44336');
+            getMyTodo();
+        }, "Twoje zadanie zostało dodane");
     }
 
     $scope.endTodo = function(todoId) {
-        var dataToSend = { token: Cookies.get('tq'), tid: todoId };
-        var urlToPost = 'backend/endTodo';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $.gritter.add({
-                        title: 'Sukces',
-                        text: 'Twoje zadanie pomyślnie zakończone',
-                        image: '',
-                        sticky: true,
-                        time: 5,
-                        class_name: 'my-sticky-class'
-                    });
-                    var last = $scope.todoList;
-                    var newTodo = [];
+        request.backend('endTodo', { tid: todoId }, function() {
+            var last = $scope.todoList;
+            var newTodo = [];
+            for (var i = 0; i < last.length; i++) {
+                if (last[i].id != todoId)
+                    newTodo.push(last[i]);
+            }
+            $scope.$apply(function() {
+                $scope.todoList = newTodo;
+            });
+        }, "Twoje zadanie pomyślnie zakończone");
+    }
 
-                    for (var i = 0; i < last.length; i++) {
-                        if (last[i].id != todoId)
-                            newTodo.push(last[i]);
-                    }
-                    $scope.$apply(function() {
-                        $scope.todoList = newTodo;
-                    });
-
-                } else {
-                    console.log(msg);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Brak todo do wyświetlenia',
-                        image: '',
-                        sticky: true,
-                        time: 5,
-                        class_name: 'my-sticky-class'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety nie udało się pobrać Twoich todo',
-                    image: '',
-                    sticky: true,
-                    time: 5,
-                    class_name: 'my-sticky-class'
-                });
-            },
-        });
+    $scope.selectColor = function(key, value) {
+        $scope.selectedColor = value;
+        $scope.selectedKey = key;
+        setInputColor();
     }
 
     function getMyTodo() {
-        var dataToSend = { token: Cookies.get('tq') };
-        var urlToPost = 'backend/getTodo';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $scope.$apply(function() {
-                        $scope.todoList = msg.data;
-                    });
-                } else {
-                    console.log(msg);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Brak todo do wyświetlenia',
-                        sticky: true,
-                        time: 5,
-                        class_name: 'my-sticky-class'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety nie udało się pobrać Twoich todo',
-                    sticky: true,
-                    time: 5,
-                    class_name: 'my-sticky-class'
-                });
-            },
+        request.backend('getTodo', {}, function(data) {
+            $scope.$apply(function() {
+                $scope.todoList = data;
+            });
         });
     }
+
+    function setInputColor() {
+        $("#todoText").css("border-color", $scope.selectedColor);
+        $("#todoButton").css("border-color", $scope.selectedColor);
+    }
+
+    $(document).on("click", "", function() {
+
+    });
 });
