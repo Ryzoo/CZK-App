20b34502ad4e12d23788d7faab2fc9fcd30d41f4
Grzegorz Kastelik <g.kastelik@gmail.com>
2017-09-02 04:48:40 +0200

    Update post module

--- modules/Post/assets/controllers/tabController.js
+++ modules/Post/assets/controllers/tabController.js
@@ -1,7 +1,8 @@
-app.controller('tabController', function($scope, auth, $rootScope, notify) {
+app.controller('tabController', function($scope, auth, $rootScope, notify,request) {
     $scope.lastId = 0;
     $scope.posts = [];
     $scope.maxPost = 5;
+    $scope.showContent = false;
 
     $scope.morePost = function() {
         $scope.maxPost += 5;
@@ -17,275 +18,63 @@ app.controller('tabController', function($scope, auth, $rootScope, notify) {
     }
 
     $scope.getLastPost = function() {
-        $rootScope.showContent = false;
-        var dataToSend = { token: Cookies.get('tq'), tmid: $rootScope.user.tmid };
-        var urlToPost = 'backend/getPost';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    if (msg.data) {
-                        scope.posts=[];
-                        for (var i = 0; i < msg.data.length; i++) {
-                            $scope.$apply(function() {
-                                $scope.posts.push(msg.data[i]);
-                                $scope.posts[i].maxComment = 1;
-                            });
-                        }
-                        if (msg.data[0] != null && msg.data[0].psid != null) $scope.lastId = msg.data[0].psid;
 
-                    }
-                    $scope.$apply(function() {
-                        $scope.showContent = true;
-                    });
-
-                } else {
-                    console.log(msg);
-                    $(document).ready(function() {
-                        var unique_id = $.gritter.add({
-                            title: 'Bład',
-                            text: 'Brak postów do wyświetlenia',
-                            image: '',
-                            sticky: true,
-                            time: 3,
-                            class_name: 'my-sticky-class'
-                        });
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $(document).ready(function() {
-                    var unique_id = $.gritter.add({
-                        title: 'Bład',
-                        text: 'Niestety nie udało się pobrać postów',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
+        request.backend('getPost', { tmid: $rootScope.user.tmid }, function(data) {
+            $scope.$apply(function() {
+                $scope.showContent = true;
+                $scope.posts=[];
+            });
+            for (var i = 0; i < data.length; i++) {
+                $scope.$apply(function() {
+                    $scope.posts.push(data[i]);
+                    $scope.posts[i].maxComment = 1;
                 });
-            },
+            }
+            if (data[0] != null && data[0].psid != null) $scope.lastId = data[0].psid;
         });
+
     }
 
     $scope.addPost = function() {
         $('#errorNewPost').html("").hide();
         var message = $("#newPostInput").val();
         if (message.length < 5 || message.length > 500) {
-            $.gritter.add({
-                title: 'Walidacja',
-                text: 'Wiadomosc musi być dłuższa niż 5 znaków oraz krótsza niż 500',
-                image: '',
-                sticky: true,
-                time: 3,
-                class_name: 'my-sticky-class'
-            });
+            notify.localNotify( 'Walidacja', 'Wiadomosc musi być dłuższa niż 5 znaków oraz krótsza niż 500');
             return;
         }
-        var dataToSend = { token: Cookies.get('tq'), msg: message, tmid: $rootScope.user.tmid };
-        var urlToPost = 'backend/addPost';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $scope.getLastPost();
-                    $.gritter.add({
-                        title: 'Dodawanie postu',
-                        text: 'Twój post został pomyślnie dodany',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                    notify.addNew(new notify.Notification($rootScope.user.firstname + " " + $rootScope.user.lastname + " dodał post", null, "#!/tab", true));
-                } else {
-                    console.log(msg.error);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Niestety coś poszło źle',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety coś poszło źle',
-                    image: '',
-                    sticky: true,
-                    time: 3,
-                    class_name: 'my-sticky-class'
-                });
-            },
-        });
+
+        request.backend('addPost', { msg: message, tmid: $rootScope.user.tmid }, function(data) {
+            $scope.getLastPost();
+            notify.addNew(new notify.Notification($rootScope.user.firstname + " " + $rootScope.user.lastname + " dodał post", null, "#!/tab", true));
+        },'Twój post został pomyślnie dodany');
     }
 
     $scope.addComment = function(psid) {
         var message = $("#tx_" + psid).val();
         if (message.length < 5 || message.length > 500) {
-            $.gritter.add({
-                title: 'Bład',
-                text: 'Komentarz musi zawierać od 5 do 500 znaków',
-                image: '',
-                sticky: true,
-                time: 3,
-                class_name: 'my-sticky-class'
-            });
+            notify.localNotify( 'Walidacja', 'Komentarz musi zawierać od 5 do 500 znaków');
             return;
         }
-        var dataToSend = { token: Cookies.get('tq'), msg: message, post_id: psid };
-        var urlToPost = 'backend/addComment';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $scope.lastId = 0;
-                    $scope.posts = [];
-                    $scope.getLastPost();
-                    $.gritter.add({
-                        title: 'Dodanie komentarza',
-                        text: 'Twój komentarz został pomyślnie dodany',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                } else {
-                    console.log(msg.error);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Niestety coś poszło źle',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety coś poszło źle',
-                    image: '',
-                    sticky: true,
-                    time: 3,
-                    class_name: 'my-sticky-class'
-                });
-            },
-        });
+
+        request.backend('addComment', {msg: message, post_id: psid }, function(data) {
+            $scope.getLastPost();
+        },'Twój komentarz został pomyślnie dodany');
     }
 
     $scope.deletePost = function(id) {
 
-        var dataToSend = { token: Cookies.get('tq'), psid: id };
-        var urlToPost = 'backend/deletePost';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $scope.lastId = 0;
-                    $scope.posts = [];
-                    $scope.getLastPost();
-                    $.gritter.add({
-                        title: 'Usuwanie posta',
-                        text: 'Post został usunięty pomyślnie',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                } else {
-                    console.log(msg.error);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Niestety coś poszło źle',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety coś poszło źle',
-                    image: '',
-                    sticky: true,
-                    time: 3,
-                    class_name: 'my-sticky-class'
-                });
-            },
-        });
+        request.backend('deletePost', {psid: id }, function(data) {
+            $scope.getLastPost();
+        },'Post został usunięty pomyślnie');
+
     }
 
     $scope.deleteComment = function(id) {
-        var dataToSend = { token: Cookies.get('tq'), cmid: id };
-        var urlToPost = 'backend/deleteComment';
-        $.ajax({
-            url: urlToPost,
-            type: "POST",
-            data: dataToSend,
-            async: true,
-            success: function(msg) {
-                if (msg.success) {
-                    $scope.lastId = 0;
-                    $scope.posts = [];
-                    $scope.getLastPost();
-                    $.gritter.add({
-                        title: 'Usuwanie komentarza',
-                        text: 'Komentarz został usunięty pomyślnie',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
-                    });
-                } else {
-                    console.log(msg);
-                    $.gritter.add({
-                        title: 'Bład',
-                        text: 'Niestety coś poszło źle',
-                        image: '',
-                        sticky: true,
-                        time: 3,
-                        class_name: 'my-sticky-class'
 
-                    });
-                }
-            },
-            error: function(jqXHR, textStatus) {
-                console.log("Blad podczas laczenia z serverem: " + textStatus);
-                $.gritter.add({
-                    title: 'Bład',
-                    text: 'Niestety coś poszło źle',
-                    image: '',
-                    sticky: true,
-                    time: 3,
-                    class_name: 'my-sticky-class'
+        request.backend('deleteComment', {cmid: id }, function(data) {
+            $scope.getLastPost();
+        },'Komentarz został usunięty pomyślnie');
 
-                });
-            },
-        });
     }
-
 
 });
