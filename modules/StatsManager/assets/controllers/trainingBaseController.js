f6e3e87de785fdf7bc1a063d21baddaebb34826a
lukasz2017 <djokus@o2.pl>
2017-10-22 13:47:50 +0100

    Add names of tests

--- modules/StatsManager/assets/controllers/trainingBaseController.js
+++ modules/StatsManager/assets/controllers/trainingBaseController.js
@@ -1,13 +1,55 @@
 app.controller('trainingBaseController', function($scope, auth, $rootScope, notify, statistic, request) {
     $rootScope.showContent = true;
     $scope.trainingBase = [{
-        name: "asda sd",
+        name: "Beep Test",
         content: "asas dasd"
     }, {
-        name: "asd asd1",
+        name: "Bieg bez piłki",
         content: "asasd asd2"
     }, {
-        name: "asd asd2",
+        name: "FMS - Mobilność barków",
+        content: "asas dasd2"
+    }, {
+        name: "FMS - Pompka",
+        content: "asas dasd2"
+    }, {
+        name: "FMS - Przejście nad płotkiem",
+        content: "asas dasd2"
+    }, {
+        name: "FMS - Przysiad głęboki",
+        content: "asas dasd2"
+    }, {
+        name: "FMS - Przysiad w wykroku",
+        content: "asas dasd2"
+    }, {
+        name: "FMS - Stabilność rotacyjna tułowia",
+        content: "asas dasd2"
+    }, {
+        name: "FMS - Uniesienie wyprostowanej nogi",
+        content: "asas dasd2"
+    }, {
+        name: "Ławeczki",
+        content: "asas dasd2"
+    }, {
+        name: "Prowadzenie piłki",
+        content: "asas dasd2"
+    }, {
+        name: "RAST",
+        content: "asas dasd2"
+    }, {
+        name: "Skok w dal z miejsca",
+        content: "asas dasd2"
+    }, {
+        name: "Szybkość (30m)",
+        content: "asas dasd2"
+    }, {
+        name: "Wyskok dosiężny",
+        content: "asas dasd2"
+    }, {
+        name: "Zwroty z piłką",
+        content: "asas dasd2"
+    }, {
+        name: "Żonglerka piłką",
         content: "asas dasd2"
     }]
 });
