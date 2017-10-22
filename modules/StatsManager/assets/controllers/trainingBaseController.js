8e11ba4f7180595941b5e186d634c04401de9c4e
Grzegorz Kastelik <g.kastelik@gmail.com>
2017-10-22 22:42:48 +0200

    Fix

--- modules/StatsManager/assets/controllers/trainingBaseController.js
+++ modules/StatsManager/assets/controllers/trainingBaseController.js
@@ -2,103 +2,103 @@ app.controller('trainingBaseController', function($scope, auth, $rootScope, noti
     $rootScope.showContent = true;
     $scope.trainingBase = [{
         name: "Beep Test",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Bieg bez piłki",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Mobilność barków",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Pompka",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Przejście nad płotkiem",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Przysiad głęboki",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Przysiad w wykroku",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Stabilność rotacyjna tułowia",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "FMS - Uniesienie wyprostowanej nogi",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Ławeczki",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Prowadzenie piłki",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "RAST",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Skok w dal z miejsca",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Szybkość (30m)",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Wyskok dosiężny",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Zwroty z piłką",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
     }, {
         name: "Żonglerka piłką",
-        age-category: "<strong>Kategoria wiekowa:</strong>",
+        age_category: "<strong>Kategoria wiekowa:</strong>",
         equipment: "<strong>Wyposażenie:</strong>",
         execution: "<strong>Wykonanie:</strong>",
         scoring: "<strong>Punktacja:</strong>"
