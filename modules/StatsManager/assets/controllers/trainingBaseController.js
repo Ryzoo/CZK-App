621f69a27351ede09479c22f9c33dd03402a5bde
lukasz2017 <djokus@o2.pl>
2017-10-23 12:24:38 +0100

    Real content try

--- modules/StatsManager/assets/controllers/trainingBaseController.js
+++ modules/StatsManager/assets/controllers/trainingBaseController.js
@@ -8,10 +8,16 @@ app.controller('trainingBaseController', function($scope, auth, $rootScope, noti
             ageCategory: "od 13 roku życia wzwyż",
             equipment: "2 linie (lub inne znaczniki), urządzenie audio do sygnalizowania (np. laptop)",
             execution: [ // wartosc tego pola jest tablica czyli []
-                "wartosc1",
-                "wartosc2",
-                "itp"
-            ]
+                "1. Linie oddalone są od siebie na odległość 20m.",
+                "2. Zawodnik staje na jednej z nich.",
+                "3. Zadaniem zawodnika jest biec od linii do linii w wyznaczonych przedziałach czasu.",
+                "4. Każdy start z linii musi być zsynchronizowany z sygnałem dźwiękowym („Beep”).",
+                "5. Zawodnik biega tak długo, dopóki nie zrezygnuje (nie da rady więcej biec) lub w chwili gdy po raz drugi nie dotrze do linii na czas.",
+                "6. Zapisywany jest ostatni etap oraz odcinek, w którym zawodnik dotarł do linii.",
+                "7. Zaczynamy od tempa 8.5km/h. Prędkość się zwiększa.",
+                "8. Jeśli zawodnik dotrze do linii za szybko, powinien poczekać na sygnał."
+            ],
+            scoring: "Im więcej zaliczonych etapów i odcinków 20-metrowych – tym lepiej. Do e-platformy Club Management Center wynik wprowadzamy w następujący sposób: liczba przed kropką oznacza etap, a po kropce – odcinek. Takim sposobem, gdy zawodnik ukończył test na 10 etapie i 2 odcinku, zapisujemy wynik jako 10.02. Analogicznie jeśli ukończył test na 9 etapie i 10 odcinku, zapisujemy wynik jako 9.1. W razie niepewności prosimy o kontakt z działem technicznym."
         }
         // teraz po przecinku dodajemy kolejne obiekty
         // w htmlu beda one wyswietlane kolejno wedlug szablonu
