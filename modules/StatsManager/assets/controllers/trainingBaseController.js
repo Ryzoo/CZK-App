0533de3b08e8489dde5c65f4179e3a4c59c577df
lukasz2017 <djokus@o2.pl>
2017-10-23 12:53:46 +0100

    Add content of second test

--- modules/StatsManager/assets/controllers/trainingBaseController.js
+++ modules/StatsManager/assets/controllers/trainingBaseController.js
@@ -22,19 +22,15 @@ app.controller('trainingBaseController', function($scope, auth, $rootScope, noti
 
         {
             name: "Bieg bez piłki",
-            ageCategory: "od 13 roku życia wzwyż",
-            equipment: "2 linie (lub inne znaczniki), urządzenie audio do sygnalizowania (np. laptop)",
+            ageCategory: "od 8 do 13 roku życia wzwyż",
+            equipment: "8 tyczek (lub innych znaczników)",
             execution: [
-                "1. Linie oddalone są od siebie na odległość 20m.",
-                "2. Zawodnik staje na jednej z nich.",
-                "3. Zadaniem zawodnika jest biec od linii do linii w wyznaczonych przedziałach czasu.",
-                "4. Każdy start z linii musi być zsynchronizowany z sygnałem dźwiękowym („Beep”).",
-                "5. Zawodnik biega tak długo, dopóki nie zrezygnuje (nie da rady więcej biec) lub w chwili gdy po raz drugi nie dotrze do linii na czas.",
-                "6. Zapisywany jest ostatni etap oraz odcinek, w którym zawodnik dotarł do linii.",
-                "7. Zaczynamy od tempa 8.5km/h. Prędkość się zwiększa.",
-                "8. Jeśli zawodnik dotrze do linii za szybko, powinien poczekać na sygnał."
+              "1. Zawodnik staje na linii startu."
+              "2. Ma przebiec po wyznaczonej trasie (obiegając każdą z tyczek) w jak najszybszym czasie."
+              "3. Rozpoczęcie biegu następuje po komendzie trenera – wtedy również zaczynamy liczenie czasu."
+              "4. Zatrzymujemy czas w momencie przebiegnięcia zawodnika przez linię mety."
             ],
-            scoring: "Im więcej zaliczonych etapów i odcinków 20-metrowych – tym lepiej. Do e-platformy Club Management Center wynik wprowadzamy w następujący sposób: liczba przed kropką oznacza etap, a po kropce – odcinek. Takim sposobem, gdy zawodnik ukończył test na 10 etapie i 2 odcinku, zapisujemy wynik jako 10.02. Analogicznie jeśli ukończył test na 9 etapie i 10 odcinku, zapisujemy wynik jako 9.1. W razie niepewności prosimy o kontakt z działem technicznym."
+            scoring: "Im krótszy czas wykonania – tym lepiej. Niepoprawne wykonanie testu oznacza dyskwalifikację. Należy wtedy powtórzyć cały test od nowa."
         }
     ]
 });
