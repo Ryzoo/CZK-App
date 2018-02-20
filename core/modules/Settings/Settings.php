ac04e29d75f02122a376d6a317e6e00dea3d6e4d
Grzegorz Kastelik <g.kastelik@gmail.com>
2018-02-20 20:54:00 +0100

    fix db

--- core/modules/Settings/Settings.php
+++ core/modules/Settings/Settings.php
@@ -161,7 +161,7 @@ class Settings extends BasicModule{
     public function getDatabaseConfig(){
         $json = new JSON();
         $dbConfig = $json->decodeFile(__DIR__. '/../../mainConf.json' );
-        return DEBUG ? $dbConfig->localDatabase : $dbConfig->database;
+        return $dbConfig->database;
     }
 
     public function getAvailableThemes(){
