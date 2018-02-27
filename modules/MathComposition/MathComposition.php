d664949d5a7a1eaf581932d52d470db0bde34da4
Grzegorz Kastelik <g.kastelik@gmail.com>
2018-02-27 19:44:53 +0100

    Update meet module

--- modules/MathComposition/MathComposition.php
+++ modules/MathComposition/MathComposition.php
@@ -15,14 +15,7 @@ class MathComposition extends BasicModule {
 
     function getNextMatchComposition($data){
         $tmid = $data['tmid'];
-        $this->returnedData['data'] = ($this->db->getConnection())->fetchRow('SELECT * FROM ma_meet WHERE DATE(date) >= DATE(NOW()) AND id_team='.$tmid.' ORDER BY date LIMIT 1');
-
-        if(isset($this->returnedData['data']))
-            if( $this->returnedData['data']['status'] == "Oczekiwanie" && (new DateTime()) > (new DateTime($this->returnedData['data']['date'])) ){
-                $this->returnedData['data']['status'] = "Zakończone";
-                ($this->db->getConnection())->update('ma_meet',["id"=>$this->returnedData['data']['id']],['status' => "Zakończone"]);
-            }
-
+        $this->returnedData['data'] = ($this->db->getConnection())->fetchRow("SELECT * FROM ma_meet WHERE status LIKE 'Oczekiwanie' AND DATE(date) >= DATE(NOW()) AND id_team='.$tmid.' ORDER BY date LIMIT 1");
         return $this->returnedData;
     }
 }
